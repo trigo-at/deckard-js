0bfe68b985dbd76643a245d84d85de8a3557c6e8
David Wippel <david@trigo.at>
2018-06-11 10:09:27 +0200

    adds missing exports

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 // atoms
 export {default as Badge} from './atoms/Badge';
 export {default as Box} from './atoms/Box';
-export {default as Card} from './atoms/Card';
-export {default as Flex} from './atoms/Flex';
-export {default as Text} from './atoms/Text';
-export {default as Heading} from './atoms/Heading';
 export {default as Button} from './atoms/Button';
-export {default as OutlineButton} from './atoms/OutlineButton';
-export {default as Input} from './atoms/Input';
-export {default as Icon} from './atoms/Icon';
+export {default as Card} from './atoms/Card';
 export {default as Container} from './atoms/Container';
+export {default as Flex} from './atoms/Flex';
+export {default as Heading} from './atoms/Heading';
+export {default as Icon} from './atoms/Icon';
+export {default as Input} from './atoms/Input';
+export {default as Label} from './atoms/Label';
+export {default as OutlineButton} from './atoms/OutlineButton';
 export {default as Select} from './atoms/Select';
+export {default as Text} from './atoms/Text';
 // molecules
-export {default as IconButton} from './molecules/IconButton';
 export {default as Banner} from './molecules/Banner';
 export {default as Checkbox} from './molecules/Checkbox';
+export {default as IconButton} from './molecules/IconButton';
 // organism
 export {default as FormField} from './organisms/FormField';
 // theme
