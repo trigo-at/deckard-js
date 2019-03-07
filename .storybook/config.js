371b062381373cf815c387aa01dfca871545b08c
David Wippel <david@trigo.at>
2019-03-07 21:21:12 +0100

    update storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import {configure, addDecorator} from '@storybook/react';
-import {setOptions} from '@storybook/addon-options';
+import {configure, addParameters, addDecorator} from '@storybook/react';
+import {create} from '@storybook/theming';
 import {withInfo} from '@storybook/addon-info';
 import ThemeProvider from '../src/ThemeProvider';
 import Box from '../src/Box';
 
-setOptions({
-    name: 'trigo Design System',
-    url: 'https://www.trigo.at',
-    hierarchySeparator: /\//,
-    hierarchyRootSeparator: /\|/,
+addParameters({
+    options: {
+        theme: create({
+            base: 'light',
+            brandTitle: 'trigo Design System',
+            brandUrl: 'https://www.trigo.at',
+            // To control appearance:
+            // brandImage: 'http://url.of/some.svg',
+        }),
+    },
 });
 
 addDecorator(withInfo);
-addDecorator(story => <ThemeProvider><Box p={3}>{story()}</Box></ThemeProvider>);
+addDecorator(story => (
+    <ThemeProvider>
+        <Box p={3}>{story()}</Box>
+    </ThemeProvider>
+));
 
 // automatically import all files ending in *.stories.jsx
 const req = require.context('../stories', true, /.stories.jsx$/);
