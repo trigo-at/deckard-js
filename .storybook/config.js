8c9a4808e9dd1a9243429cb34c8e164b56e1a216
David Wippel <david@trigo.at>
2019-03-07 21:37:42 +0100

    update storybook config

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,14 +5,15 @@ import {withInfo} from '@storybook/addon-info';
 import ThemeProvider from '../src/ThemeProvider';
 import Box from '../src/Box';
 
+import logo from '../src/logo.png';
+
 addParameters({
     options: {
         theme: create({
             base: 'light',
             brandTitle: 'trigo Design System',
             brandUrl: 'https://www.trigo.at',
-            // To control appearance:
-            // brandImage: 'http://url.of/some.svg',
+            brandImage: logo,
         }),
     },
 });
