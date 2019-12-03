fb854f9c68ac3c3f68e20df1a43a15693ea7f2d4
David Wippel <david@trigo.at>
2019-12-03 21:47:31 +0100

    chore: update docs

---
+++ docs/main.6df70a61ceb805c5a432.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{0:function(n,o,c){c(441),c(553),c(985),n.exports=c(987)},1:function(n,o){},553:function(n,o,c){"use strict";c.r(o);c(554),c(813),c(816),c(854),c(878)}},[[0,1,2]]]);
