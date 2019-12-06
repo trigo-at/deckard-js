6601b3e9490d977c67081548fc05bd1a74421db1
David Wippel <david@trigo.at>
2019-12-06 12:12:00 +0100

    feat(number-field): added NumberField component

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ export {default as InputField} from './form-fields/input-field';
 export {default as RadioGroupField} from './form-fields/radio-group-field';
 export {default as SelectField} from './form-fields/select-field';
 export {default as TextareaField} from './form-fields/textarea-field';
+export {default as NumberField} from './form-fields/number-field';
 
 // Inidcators
 export {default as ErrorIndicator} from './indicators/error-indicator';
