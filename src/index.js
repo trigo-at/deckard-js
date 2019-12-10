9204bc724deebb9e9a7036467be5556a29468f8c
David Wippel <david@trigo.at>
2019-12-10 09:26:32 +0100

    feat(date): added toDateTimeString utility function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export {Link as RouterLink} from '@reach/router';
 export {FormattedMessage, useIntl} from 'react-intl';
 export {default as ThemeProvider} from './theme-provider';
 export {default as toDateString} from './to-date-string';
+export {default as toDateTimeString} from './to-date-time-string';
 
 // Form fields
 export {default as CheckboxField} from './form-fields/checkbox-field';
---
+++ src/to-date-time-string.js
@@ -0,0 +1,5 @@
+import parseISO from 'date-fns/parseISO';
+import format from 'date-fns/format';
+
+export default dateField =>
+    dateField ? format(parseISO(dateField), 'dd.MM.yyyy HH:mm') : undefined;
