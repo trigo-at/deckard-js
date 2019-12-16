48cdf74f1de3abd9b471e7ca8bb0b94ea296eb38
David Wippel <david@trigo.at>
2019-12-16 21:12:27 +0100

    fix(user-menu): onLogout is optional

--- src/layout/user-menu.jsx
+++ src/layout/user-menu.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import {string, func} from 'prop-types';
 import {FormattedMessage} from 'react-intl';
-import {Menu, MenuButton, MenuList, MenuItem, Button} from '@chakra-ui/core';
+import {
+    Text,
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Button,
+} from '@chakra-ui/core';
 
 const UserMenu = ({userName, onLogout}) => {
+    if (!onLogout) {
+        return <Text>{userName}</Text>;
+    }
+
     return (
         <Menu>
             <MenuButton as={Button} variant="ghost" rightIcon="chevron-down">
                 {userName}
             </MenuButton>
             <MenuList>
-                {onLogout && (
-                    <MenuItem onClick={onLogout}>
-                        <FormattedMessage id="common.logout" />
-                    </MenuItem>
-                )}
+                <MenuItem onClick={onLogout}>
+                    <FormattedMessage id="common.logout" />
+                </MenuItem>
             </MenuList>
         </Menu>
     );
