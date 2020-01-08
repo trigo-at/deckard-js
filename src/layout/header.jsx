3c1a7ede460db849e10008881b44a36671730c9f
David Wippel <david@trigo.at>
2020-01-08 13:10:35 +0100

    feat: remove avatar (not used)

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {string, func} from 'prop-types';
-import {Flex, Box, Avatar} from '@chakra-ui/core';
+import {Flex, Box} from '@chakra-ui/core';
 import SearchBox from './search-box';
 import UserMenu from './user-menu';
 
-const Header = ({userName, avatarSrc, onSearch, onLogout}) => {
+const Header = ({userName, onSearch, onLogout}) => {
     return (
         <Flex alignItems="center" justifyContent="space-between" bg="gray.50">
             <Box flexGrow="1" px={6}>
@@ -15,7 +15,6 @@ const Header = ({userName, avatarSrc, onSearch, onLogout}) => {
                 flexGrow="1"
                 justifyContent="flex-end"
                 alignItems="center">
-                <Avatar mx={2} name={userName} src={avatarSrc} />
                 <UserMenu userName={userName} onLogout={onLogout} />
             </Flex>
         </Flex>
@@ -24,14 +23,12 @@ const Header = ({userName, avatarSrc, onSearch, onLogout}) => {
 
 Header.propTypes = {
     userName: string,
-    avatarSrc: string,
     onSearch: func,
     onLogout: func,
 };
 
 Header.defaultProps = {
     userName: undefined,
-    avatarSrc: undefined,
     onSearch: undefined,
     onLogout: undefined,
 };
