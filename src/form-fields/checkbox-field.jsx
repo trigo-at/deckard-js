5e9c98f76d2cda5f430b84b210a855189d384f8e
David Wippel <david@trigo.at>
2020-01-17 12:28:11 +0100

    feat(checkbox-field): add forwardRef

--- src/form-fields/checkbox-field.jsx
+++ src/form-fields/checkbox-field.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
 import {string, node} from 'prop-types';
 import {useField} from 'react-final-form';
 import {FormControl, Checkbox, FormHelperText} from '@chakra-ui/core';
 import {FormattedMessage} from 'react-intl';
 import FieldError from './field-error';
 
-const CheckboxField = ({name, gridArea, fieldName, helperText, ...props}) => {
-    const {input, meta} = useField(name, {type: 'checkbox'});
-    const isInvalid =
-        (!!meta.error && meta.touched) ||
-        (!!meta.submitError && !meta.dirtySinceLastSubmit && !meta.submitting);
-    return (
-        <FormControl gridArea={gridArea || name} isInvalid={isInvalid}>
-            <Checkbox {...input} {...props}>
-                <FormattedMessage id={`field.${fieldName || name}`} />
-            </Checkbox>
-            {helperText && (
-                <FormHelperText>
-                    <FormattedMessage id={helperText} />
-                </FormHelperText>
-            )}
-            <FieldError isInvalid={isInvalid} meta={meta} />
-        </FormControl>
-    );
-};
+const CheckboxField = forwardRef(
+    ({name, gridArea, fieldName, helperText, ...props}, ref) => {
+        const {input, meta} = useField(name, {type: 'checkbox'});
+        const isInvalid =
+            (!!meta.error && meta.touched) ||
+            (!!meta.submitError &&
+                !meta.dirtySinceLastSubmit &&
+                !meta.submitting);
+        return (
+            <FormControl gridArea={gridArea || name} isInvalid={isInvalid}>
+                <Checkbox {...input} {...props} ref={ref}>
+                    <FormattedMessage id={`field.${fieldName || name}`} />
+                </Checkbox>
+                {helperText && (
+                    <FormHelperText>
+                        <FormattedMessage id={helperText} />
+                    </FormHelperText>
+                )}
+                <FieldError isInvalid={isInvalid} meta={meta} />
+            </FormControl>
+        );
+    }
+);
 
 CheckboxField.propTypes = {
     name: string.isRequired,
