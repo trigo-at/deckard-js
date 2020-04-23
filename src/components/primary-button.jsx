e68f543cef32c4b759dd99bdd3c6cb1ef90afeb1
David Wippel <david@trigo.at>
2020-04-23 14:14:48 +0200

    fix: fix primary button color

--- src/components/primary-button.jsx
+++ src/components/primary-button.jsx
@@ -2,15 +2,7 @@ import React, {forwardRef} from 'react';
 import {Button} from '@chakra-ui/core';
 
 const PrimaryButton = forwardRef((props, ref) => {
-    return (
-        <Button
-            ref={ref}
-            variantColor="teal"
-            color="white"
-            variant="solid"
-            {...props}
-        />
-    );
+    return <Button ref={ref} variantColor="teal" variant="solid" {...props} />;
 });
 
 PrimaryButton.propTypes = {};
