3bbbe8deb78c171d4a28a60a82ca1fb78f4677de
philippmossier <mossier.dev@gmail.com>
2020-07-17 18:21:13 +0200

    (100): migrate chakra page-section to tailwind/ts version

---
+++ src/patterns/page-section.tsx
@@ -0,0 +1,47 @@
+import React, {SFC, ReactNode} from 'react';
+import {TranslationMessage, TranslationValues} from 'src/types';
+import {FormattedMessage} from 'react-intl';
+
+// Questions:   1.  Check if bg white is really needed
+//              2.  Check if its necessary to use a Transaltion libary like FormattedMessage/react-intl?
+//                  Maybe there is a lightweight vanilla js-solution for it to reduce external dependencies.
+
+type Props = {
+    title?: TranslationMessage;
+    values?: TranslationValues;
+    actions?: ReactNode;
+    children: ReactNode;
+    isPrimary: boolean;
+};
+
+const PageSection: SFC<Props> = ({
+    title,
+    values,
+    actions,
+    children,
+    isPrimary,
+}: Props): React.ReactElement => {
+    return (
+        <section className="mb-8">
+            <div className="flex justify-between items-center">
+                {isPrimary ? (
+                    <h1 className="font-bold mb-4 mt-4 text-lg">
+                        <FormattedMessage id={title} values={values} />
+                    </h1>
+                ) : (
+                    <h2 className="font-bold mb-4 mt-4 text-lg">
+                        <FormattedMessage id={title} values={values} />
+                    </h2>
+                )}
+
+                {actions && <div>{actions}</div>}
+            </div>
+            <div
+                className="bg-white rounded shadow-md p-4" // bg white needed ? seems not needed for me
+            >
+                {children}
+            </div>
+        </section>
+    );
+};
+export default PageSection;
