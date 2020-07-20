b97573cfdff10190541504909aecf53f35308843
Philipp Mossier <mossier.dev@gmail.com>
2020-07-20 14:28:23 +0200

    (100): migrate chakra page-section to tailwind/ts version (#18)
    * (100): migrate chakra page-section to tailwind/ts version
    
    * (pm100): cleanup
    
    * (pm100): cleanup2
    
    * (pm100): cleanup3
    
    * add viewsection to mainstory
    
    * (pm-100): final cleanup, hopefully... :-)
    
    * (pm-100): fix isPrimary heading size
    
    * add heading to story 2

---
+++ src/patterns/page-section.tsx
@@ -0,0 +1,39 @@
+import React, {SFC, ReactNode} from 'react';
+import {TranslationMessage, TranslationValues} from 'src/types';
+import {FormattedMessage} from 'react-intl';
+
+type Props = {
+    title?: TranslationMessage;
+    values?: TranslationValues;
+    actions?: ReactNode;
+    children: ReactNode;
+    isPrimary?: boolean;
+};
+
+const PageSection: SFC<Props> = ({
+    title,
+    values,
+    actions,
+    children,
+    isPrimary,
+}: Props): React.ReactElement => {
+    return (
+        <section className="mb-8">
+            <div className="flex justify-between items-center">
+                {isPrimary ? (
+                    <h1 className="font-bold mb-4 mt-4 text-lg">
+                        <FormattedMessage id={title} values={values} />
+                    </h1>
+                ) : (
+                    <h2 className="font-bold mb-4 mt-4 text-md">
+                        <FormattedMessage id={title} values={values} />
+                    </h2>
+                )}
+
+                {actions && <div>{actions}</div>}
+            </div>
+            <div className="bg-white rounded shadow-md p-4">{children}</div>
+        </section>
+    );
+};
+export default PageSection;
