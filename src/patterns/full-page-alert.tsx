10fbceb0662ad5397f6b2aea592f1565cd828eff
philippmossier <mossier.dev@gmail.com>
2020-07-23 09:58:50 +0200

    (feat/3179): solve purge mechanism bug with bg-color

--- src/patterns/full-page-alert.tsx
+++ src/patterns/full-page-alert.tsx
@@ -14,19 +14,19 @@ type Props = {
 
 const STATUSES = {
     success: {
-        backgroundColor: 'green-200',
+        backgroundColor: 'bg-green-200',
         icon: <Icon mt={5} size="40px" name="check-circle" color="green.500" />,
     },
     error: {
-        backgroundColor: 'red-200',
+        backgroundColor: 'bg-red-200',
         icon: <Icon mt={5} size="40px" name="warning" color="red.500" />,
     },
     warning: {
-        backgroundColor: 'orange-200',
+        backgroundColor: 'bg-orange-200',
         icon: <Icon mt={5} size="40px" name="warning-2" color="orange.500" />,
     },
     info: {
-        backgroundColor: 'blue-200',
+        backgroundColor: 'bg-blue-200',
         icon: <Icon mt={5} size="40px" name="info" color="blue.500" />,
     },
 };
@@ -41,7 +41,7 @@ const FullPageAlert: SFC<Props> = ({
     return (
         <>
             <div
-                className={`flex flex-col items-center text-center bg-${STATUSES[status].backgroundColor} h-60`}>
+                className={`flex flex-col items-center text-center h-60 ${STATUSES[status].backgroundColor}`}>
                 {STATUSES[status].icon}
                 <div className="mt-4 mb-2 font-bold text-2xl">
                     <FormattedMessage id={title} />
