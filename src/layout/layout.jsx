043bf53713be4b402bc422348ecf0f4e9fa0174d
David Wippel <david@trigo.at>
2020-08-17 23:24:57 +0200

    fix: fixes some layout issues

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -17,12 +17,12 @@ const Layout = ({header, sidebar, children}) => {
                     className="flex-1 relative z-0 overflow-y-auto focus:outline-none"
                     tabIndex="0">
                     <div className="pt-2 pb-6 md:py-6">
-                        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                        <div className="mx-auto px-4 sm:px-6 md:px-8">
                             <h1 className="text-2xl font-semibold text-gray-900">
                                 Dashboard
                             </h1>
                         </div>
-                        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                        <div className="mx-auto px-4 sm:px-6 md:px-8">
                             {children}
                         </div>
                     </div>
