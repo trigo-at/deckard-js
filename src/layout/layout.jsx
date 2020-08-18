9a3b1b4b62a44f819e4f92c9fc5b6ecc540083a3
David Wippel <david@trigo.at>
2020-08-18 11:16:37 +0200

    fix: removes Dashboard page header

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -18,11 +18,6 @@ const Layout = ({header, sidebar, children}) => {
                     tabIndex="0">
                     <div className="pt-2 pb-6 md:py-6">
                         <div className="mx-auto px-4 sm:px-6 md:px-8">
-                            <h1 className="text-2xl font-semibold text-gray-900">
-                                Dashboard
-                            </h1>
-                        </div>
-                        <div className="mx-auto px-4 sm:px-6 md:px-8">
                             {children}
                         </div>
                     </div>
