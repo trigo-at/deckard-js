fb68a0eaabaa50c534db19b29a4df928ca3043fc
David Wippel <david@trigo.at>
2020-08-18 09:56:57 +0200

    feat: adds more icons

--- src/components/icon.jsx
+++ src/components/icon.jsx
@@ -39,6 +39,15 @@ const getPath = (icon) => {
                     d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"
                 />
             );
+        case 'database':
+            return (
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+                />
+            );
         default:
             return (
                 <path
