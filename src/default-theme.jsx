f75a376a107999a6b94db7bb90273d84976aa83d
Wolfgang Beer <wolfgang@trigo.at>
2021-01-29 13:10:30 +0100

    feat: update theme colors with tailwind color schema

---
+++ src/default-theme.jsx
@@ -0,0 +1,258 @@
+import React from 'react';
+import {theme as chakraTheme} from '@chakra-ui/core';
+
+const customIcons = {
+    home: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    search: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    add: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M11 9V5H9v4H5v2h4v4h2v-4h4V9h-4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    mobileDevices: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M17 6V5h-2V2H3v14h5v4h3.25H11a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6zm-5.75 14H3a2 2 0 0 1-2-2V2c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2v4a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-5.75zM11 8v8h6V8h-6zm3 11a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    moodSad: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zM6.5 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm7 0a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm2.16 6a6 6 0 0 0-11.32 0h11.32z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    wrench: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M6.47 9.8A5 5 0 0 1 .2 3.22l3.95 3.95 2.82-2.83L3.03.41a5 5 0 0 1 6.4 6.68l10 10-2.83 2.83L6.47 9.8z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    userGroup: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M7 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0 1c2.15 0 4.2.4 6.1 1.09L12 16h-1.25L10 20H4l-.75-4H2L.9 10.09A17.93 17.93 0 0 1 7 9zm8.31.17c1.32.18 2.59.48 3.8.92L18 16h-1.25L16 20h-3.96l.37-2h1.25l1.65-8.83zM13 0a4 4 0 1 1-1.33 7.76 5.96 5.96 0 0 0 0-7.52C12.1.1 12.53 0 13 0z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    close: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zM11.4 10l2.83-2.83-1.41-1.41L10 8.59 7.17 5.76 5.76 7.17 8.59 10l-2.83 2.83 1.41 1.41L10 11.41l2.83 2.83 1.41-1.41L11.41 10z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    save: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M0 2C0 .9.9 0 2 0h14l4 4v14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm5 0v6h10V2H5zm6 1h3v4h-3V3z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    userAdd: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M2 6H0v2h2v2h2V8h2V6H4V4H2v2zm7 0a3 3 0 0 1 6 0v2a3 3 0 0 1-6 0V6zm11 9.14A15.93 15.93 0 0 0 12 13c-2.91 0-5.65.78-8 2.14V18h16v-2.86z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    listBullet: {
+        path: (
+            <path
+                fill="currentColor"
+                d="M1 4h2v2H1V4zm4 0h14v2H5V4zM1 9h2v2H1V9zm4 0h14v2H5V9zm-4 5h2v2H1v-2zm4 0h14v2H5v-2z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+    'dots-horizontal': {
+        path: (
+            <path
+                fill="currentColor"
+                d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"
+            />
+        ),
+
+        viewBox: '0 0 20 20',
+    },
+    'dots-vertical': {
+        path: (
+            <path
+                fill="currentColor"
+                d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"
+            />
+        ),
+        viewBox: '0 0 20 20',
+    },
+};
+
+const colors = {
+    brand: {
+        blue: '#3AB2D2',
+        green: '#7FBD42',
+        yellow: '#F7CA10',
+    },
+    gray: {
+        50: '#F9FAFB',
+        100: '#F3F4F6',
+        200: '#E5E7EB',
+        300: '#D1D5DB',
+        400: '#9CA3AF',
+        500: '#6B7280',
+        600: '#4B5563',
+        700: '#374151',
+        800: '#1F2937',
+        900: '#111827',
+    },
+    red: {
+        50: '#FEF2F2',
+        100: '#FEE2E2',
+        200: '#FECACA',
+        300: '#FCA5A5',
+        400: '#F87171',
+        500: '#EF4444',
+        600: '#DC2626',
+        700: '#B91C1C',
+        800: '#991B1B',
+        900: '#7F1D1D',
+    },
+    yellow: {
+        50: '#FFFBEB',
+        100: '#FEF3C7',
+        200: '#FDE68A',
+        300: '#FCD34D',
+        400: '#FBBF24',
+        500: '#F59E0B',
+        600: '#D97706',
+        700: '#B45309',
+        800: '#92400E',
+        900: '#78350F',
+    },
+    orange: {
+        50: '#FFF7ED',
+        100: '#FFEDD5',
+        200: '#FED7AA',
+        300: '#FDBA74',
+        400: '#FB923C',
+        500: '#F97316',
+        600: '#EA580C',
+        700: '#C2410C',
+        800: '#9A3412',
+        900: '#7C2D12',
+    },
+    green: {
+        50: '#ECFDF5',
+        100: '#D1FAE5',
+        200: '#A7F3D0',
+        300: '#6EE7B7',
+        400: '#34D399',
+        500: '#10B981',
+        600: '#059669',
+        700: '#047857',
+        800: '#065F46',
+        900: '#064E3B',
+    },
+    blue: {
+        50: '#EFF6FF',
+        100: '#DBEAFE',
+        200: '#BFDBFE',
+        300: '#93C5FD',
+        400: '#60A5FA',
+        500: '#3B82F6',
+        600: '#2563EB',
+        700: '#1D4ED8',
+        800: '#1E40AF',
+        900: '#1E3A8A',
+    },
+    indigo: {
+        50: '#EEF2FF',
+        100: '#E0E7FF',
+        200: '#C7D2FE',
+        300: '#A5B4FC',
+        400: '#818CF8',
+        500: '#6366F1',
+        600: '#4F46E5',
+        700: '#4338CA',
+        800: '#3730A3',
+        900: '#312E81',
+    },
+    purple: {
+        50: '#F5F3FF',
+        100: '#EDE9FE',
+        200: '#DDD6FE',
+        300: '#C4B5FD',
+        400: '#A78BFA',
+        500: '#8B5CF6',
+        600: '#7C3AED',
+        700: '#6D28D9',
+        800: '#5B21B6',
+        900: '#4C1D95',
+    },
+    pink: {
+        50: '#FDF2F8',
+        100: '#FCE7F3',
+        200: '#FBCFE8',
+        300: '#F9A8D4',
+        400: '#F472B6',
+        500: '#EC4899',
+        600: '#DB2777',
+        700: '#BE185D',
+        800: '#9D174D',
+        900: '#831843',
+    },
+};
+
+export default {
+    ...chakraTheme,
+    fonts: {
+        body: 'Inter',
+        heading: 'Inter',
+        mono: 'Menlo, monospace',
+    },
+    colors: {
+        ...chakraTheme.colors,
+        ...colors,
+        primary: colors.indigo,
+        secondary: colors.pink,
+    },
+    icons: {
+        ...chakraTheme.icons,
+        ...customIcons,
+    },
+};
---
+++ src/documentation/04-design-tokens.stories.tsx
@@ -0,0 +1,90 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable import/prefer-default-export */
+import React from 'react';
+import {Box, Heading, Stack} from '@chakra-ui/core';
+import ThemeProvider from '../theme-provider';
+import theme from '../default-theme';
+
+const ColorBox = ({
+    themeKey,
+    themeValue,
+}: {
+    themeKey: string;
+    themeValue: string;
+}) => {
+    return (
+        <Box display="flex" flexDirection="column" width="9%" boxShadow="sm">
+            <Box bg={themeKey} height="50px" />
+            <Box px={2} py={1}>
+                <Box fontSize="9px" fontWeight="800">
+                    {themeKey}
+                </Box>
+                <Box fontSize="12px" fontFamily="mono">
+                    {themeValue}
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+const ColorGroup = ({
+    heading,
+    colorKey,
+}: {
+    heading: string;
+    colorKey: React.ReactNode;
+}) => {
+    return (
+        <>
+            <Heading fontSize="xl" p={2}>
+                {heading}
+            </Heading>
+            <Box display="flex" justifyContent="space-between">
+                {Object.keys(theme.colors[colorKey]).map((key) => {
+                    return (
+                        <ColorBox
+                            key={`${colorKey}.${key}`}
+                            themeKey={`${colorKey}.${key}`}
+                            themeValue={theme.colors[colorKey][key]}
+                        />
+                    );
+                })}
+            </Box>
+        </>
+    );
+};
+
+export const PrimaryColor = () => (
+    <ThemeProvider>
+        <ColorGroup heading="Indigo" colorKey="primary" />
+    </ThemeProvider>
+);
+
+PrimaryColor.storyName = 'PrimaryColor';
+
+export const SecondaryColor = () => (
+    <ThemeProvider>
+        <ColorGroup heading="Pink" colorKey="secondary" />
+    </ThemeProvider>
+);
+
+SecondaryColor.storyName = 'SecondaryColor';
+
+export const AllColors = () => (
+    <ThemeProvider>
+        <Stack spacing={6} shouldWrapChildren>
+            <ColorGroup heading="Gray" colorKey="gray" />
+            <ColorGroup heading="Red" colorKey="red" />
+            <ColorGroup heading="Yellow" colorKey="yellow" />
+            <ColorGroup heading="Orange" colorKey="orange" />
+            <ColorGroup heading="Green" colorKey="green" />
+            <ColorGroup heading="Blue" colorKey="blue" />
+            <ColorGroup heading="Indigo" colorKey="indigo" />
+            <ColorGroup heading="Purple" colorKey="purple" />
+            <ColorGroup heading="Pink" colorKey="pink" />
+            <ColorGroup heading="Teal" colorKey="teal" />
+        </Stack>
+    </ThemeProvider>
+);
+
+AllColors.storyName = 'AllColors';
