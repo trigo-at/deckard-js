86d0868e8fe8bf6d246ac77cadfb81cfe03c136d
David Wippel <david@trigo.at>
2021-04-08 08:59:20 +0200

    feat: update chakra, adds TableContainer to DataTable reference implementation

--- src/table/data-table.tsx
+++ src/table/data-table.tsx
@@ -1,6 +1,16 @@
 /* eslint-disable no-nested-ternary */
 import React, {useMemo, FC} from 'react';
-import {Thead, Tbody, Tr, Th, Td, chakra, Table, Stack} from '@chakra-ui/react';
+import {
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    chakra,
+    Table,
+    Stack,
+    TableContainer,
+} from '@chakra-ui/react';
 import {
     useTable,
     useSortBy,
@@ -56,52 +66,54 @@ export const DataTable: FC<DataTableProps> = ({columns, data}) => {
                 globalFilter={globalFilter}
                 setGlobalFilter={setGlobalFilter}
             />
-            <Table
-                variant="striped"
-                colorScheme="gray"
-                size="sm"
-                {...getTableProps()}>
-                <Thead>
-                    {headerGroups.map((headerGroup) => (
-                        <Tr {...headerGroup.getHeaderGroupProps()}>
-                            {headerGroup.headers.map((column) => (
-                                <Th
-                                    {...column.getHeaderProps(
-                                        column.getSortByToggleProps()
-                                    )}
-                                    isNumeric={column.isNumeric}>
-                                    {column.render('Header')}
-                                    <chakra.span pl="4">
-                                        {column.isSorted ? (
-                                            column.isSortedDesc ? (
-                                                <SortDescending aria-label="sorted descending" />
-                                            ) : (
-                                                <SortAscending aria-label="sorted ascending" />
-                                            )
-                                        ) : null}
-                                    </chakra.span>
-                                </Th>
-                            ))}
-                        </Tr>
-                    ))}
-                </Thead>
-                <Tbody {...getTableBodyProps()}>
-                    {page.map((row) => {
-                        prepareRow(row);
-                        return (
-                            <Tr {...row.getRowProps()}>
-                                {row.cells.map((cell) => (
-                                    <Td
-                                        {...cell.getCellProps()}
-                                        isNumeric={cell.column.isNumeric}>
-                                        {cell.render('Cell')}
-                                    </Td>
+            <TableContainer>
+                <Table
+                    variant="striped"
+                    colorScheme="gray"
+                    size="sm"
+                    {...getTableProps()}>
+                    <Thead>
+                        {headerGroups.map((headerGroup) => (
+                            <Tr {...headerGroup.getHeaderGroupProps()}>
+                                {headerGroup.headers.map((column) => (
+                                    <Th
+                                        {...column.getHeaderProps(
+                                            column.getSortByToggleProps()
+                                        )}
+                                        isNumeric={column.isNumeric}>
+                                        {column.render('Header')}
+                                        <chakra.span pl="4">
+                                            {column.isSorted ? (
+                                                column.isSortedDesc ? (
+                                                    <SortDescending aria-label="sorted descending" />
+                                                ) : (
+                                                    <SortAscending aria-label="sorted ascending" />
+                                                )
+                                            ) : null}
+                                        </chakra.span>
+                                    </Th>
                                 ))}
                             </Tr>
-                        );
-                    })}
-                </Tbody>
-            </Table>
+                        ))}
+                    </Thead>
+                    <Tbody {...getTableBodyProps()}>
+                        {page.map((row) => {
+                            prepareRow(row);
+                            return (
+                                <Tr {...row.getRowProps()}>
+                                    {row.cells.map((cell) => (
+                                        <Td
+                                            {...cell.getCellProps()}
+                                            isNumeric={cell.column.isNumeric}>
+                                            {cell.render('Cell')}
+                                        </Td>
+                                    ))}
+                                </Tr>
+                            );
+                        })}
+                    </Tbody>
+                </Table>
+            </TableContainer>
             <Pagination
                 pageIndex={pageIndex}
                 pageCount={pageCount}
